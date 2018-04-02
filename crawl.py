b7128e415664d1392d2472562f80e0d370b88b79
cgDeepLearn <cglearningnow@163.com>
2018-04-02 16:01:21 +0800

    [add] requirements

---
+++ crawl.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2018/4/2 15:58
+# @Author  : cgDeepLearn
+# @Email   : cglearningnow@163.com
+# @File    : crawl.py
+# @Function: crawl class
+
+import asyncio
